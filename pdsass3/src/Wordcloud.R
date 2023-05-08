0566fdd658e24e5b0123b0ec839ca27a1c969b4b
NikhilGande <126309818+NikhilGande@users.noreply.github.com>
2023-05-08 17:59:02 -0500

    Add files via upload
    added updated code file

--- pdsass3/src/Wordcloud.R
+++ pdsass3/src/Wordcloud.R
@@ -14,22 +14,37 @@ library(wordcloud)
 library(SnowballC)
 
 #Loading data
-Data<- read.csv("/Users/nikhilgande/Documents/pds3/Corona_NLP_test.csv")
+Data<- read.csv("/Users/nikhilgande/Documents/pdsass3/Corona_NLP_test.csv")
 print(Data)
 
 
-# Tokenization(converting text to tokens)
+# Tokenization(converting text to tokens) and performing stop word removal
 tokens <- Data %>%
   unnest_tokens(word, OriginalTweet) %>% anti_join(get_stopwords(),by="word")
 
+
+
 print(tokens)
 
-tokens %>% count(UserName,word,sort="TRUE")
-
-# performing stop word removal
+#counting word frequencies
 
 
-tokens %>% wordcloud( min.freq = 1,max.words = 200, scale=c(8,1),random.color = TRUE)
+frequency<- tokens %>%
+  count(word, sort = TRUE)
+
+
+#word_freq<-tokens
+print(frequency)
+
+#creating word cloud 
+png("/Users/nikhilgande/Documents/pdsass3/results/wordcloud.png", width = 800, height = 800, units = "px")
+wordcloud(words = frequency$word, freq = frequency$n, min.freq = 1,
+          max.words = 200, random.order = FALSE, rot.per = 0.35,
+          colors = brewer.pal(8, "Dark2"))
+
+
+
+
 
 
 
